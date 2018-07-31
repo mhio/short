1c5bbb5d2773454e8d771124c6de4235d3a8b739
mhio <37205532+mhio@users.noreply.github.com>
2018-07-31 22:28:24 +0000

    Docker

---
+++ docker.sh
@@ -0,0 +1,27 @@
+
+# This is this docker shortener
+function d(){
+  #set -x
+  local arg args
+  arg=$1
+  shift
+
+  case $arg in
+    b|bu*)    docker build "$@";;
+    r|ru*)    docker run "$@";;
+    c|co*)    docker container "$@";;
+    i|im*)    docker image "$@";;
+    n|ne*)    docker network "$@";;
+    v|vo*)    docker volume "$@";;
+    psa)      docker ps -a "$@";;
+    rmin)     d_images=$(docker images | awk '/<none>/ { print $3 }')
+              [ -n "$d_images" ] && docker rmi $d_images
+    ;;
+    rme)      d_containers=$(docker ps -a | awk '/Exited|Created/ { print $1 }')
+              [ -n "$d_containers" ] && docker rm $d_containers
+    ;;
+    h) echo "the (d)ocker function - d (b r c i n v)"
+       echo " d b(uild)"
+       echo " d r(un)"
+}
+
