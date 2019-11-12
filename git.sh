c3b44b5b6367c44f7062eba4df1a22a56e37fc31
mhio <37205532+mhio@users.noreply.github.com>
2019-11-12 12:49:50 +1100

    fix push

--- git.sh
+++ git.sh
@@ -12,18 +12,20 @@ function g(){
     c|co*) git commit "$@";;
     d|di*) git diff "$@";;
     l|lo*) git log "$@";;
-    p|pul*) git pull "$@";;
-    psh|pus*) git push "$@";;
+    p|pu|pul*) git pull "$@";;
+    ps|psh|pus*) git push "$@";;
     s|st*) git status "$@";;
-    h) echo "the (g)it function - g (a b c d l s)"
+    h) echo "the (g)it function - g (a b c d l p ps s)"
        echo " g a(dd)"
        echo " g b(ranch)"
        echo " g c(ommit)"
        echo " g d(iff)"
        echo " g l(og)"
        echo " g p(ull)"
-       echo " g psh(ush)"
-       echo " g s(tatus)";;
+       echo " g ps(h)"
+       echo " g s(tatus)"
+       echo
+       echo "All unkown or extra paramaters are passed on to git;;
     *) git "$@";; 
   esac
   #set +x
