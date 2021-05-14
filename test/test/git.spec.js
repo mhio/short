3b12c9e5776cf70986e27e246e86e594506613c2
mhio <37205532+mhio@users.noreply.github.com>
2021-05-14 06:49:33 +0000

    test success

--- test/test/git.spec.js
+++ test/test/git.spec.js
@@ -4,11 +4,11 @@ const debug = require('debug')('mhio:test:short:git')
 describe('unit::short::git', function(){
 
   it('should output test to stdout from binary', function(){
-    return Cli.run(['../g', 'help']).then(result => {
+    return Cli.run(['../g', 's']).then(result => {
       expect(result).to.have.property('errors').and.to.eql([])
       debug('bin out', result.stdout)
       debug('bin err', result.stderr)
-      expect( result.stdout[0] ).to.equal( 'test\n' )
+      expect( result.stdout[0] ).to.contain( 'On branch' )
       expect( result.stderr ).to.have.length(0)
       expect(result).to.have.property('exit_code').and.equal(0)
     })
