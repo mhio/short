3b10b75130ea330034f6c4a7c851f2d458699d7e
mhio <37205532+mhio@users.noreply.github.com>
2022-05-20 06:39:12 +0000

    make git sh

--- git.sh
+++ git.sh
@@ -1,38 +1,38 @@
-
 # This is this git shortener
-function g(){
-  #set -x
-  local arg
-  arg=$1
-  [ -z "$arg" ] || shift
 
-  case $arg in
-    a|ad|add) git add "$@";;
-    b|br|bra*) git branch "$@";;
-    c|co|com*) git commit "$@";;
-    ch|che*) git checkout "$@";;
-    clp) gitClonePublic "$@";;
-    d|di|dif|diff) git diff "$@";;
-    l|lo|log) git log "$@";;
-    p|pu|pul|pull) git pull "$@";;
-    ps|psh|pus|push) git push "$@";;
-    s|st|sta|stat*) git status "$@";;
-    h) echo "the (g)it function - g (a b c d l p ps s)"
+# shellcheck shell=sh
+
+gitShort(){
+  local_git_short_arg="$1"
+  [ -z "$local_git_short_arg" ] || shift
+
+  case "$local_git_short_arg" in
+    a|ad) git add "$@";;
+    b|br|bra|bran|branc) git branch "$@";;
+    c|co|com|comm|commi) git commit "$@";;
+    ch|che|chec|check|checko|checkou) git checkout "$@";;
+    d|di|dif) git diff "$@";;
+    l|lo) git log "$@";;
+    p|pu|pul) git pull "$@";;
+    ps|psh|pus) git push "$@";;
+    s|st|sta|stat|statu) git status "$@";;
+    t|ta) git tag "$@";;
+    h) echo "the (g)it function - g (a b c ch d l p ps s t)"
        echo " g a(dd)"
        echo " g b(ranch)"
        echo " g c(ommit)"
        echo " g ch(eckout)"
-       echo " g clp (clone public with full path)"
        echo " g d(iff)"
        echo " g l(og)"
        echo " g p(ull)"
        echo " g ps(h)"
        echo " g s(tatus)"
+       echo " g t(ag)"
        echo
        echo "All unkown or extra paramaters are passed on to git";;
-    *) git "$arg" "$@";; 
+    *) git "$local_git_short_arg" "$@";; 
   esac
   #set +x
 }
 
-# MIT License Copyright (c) 2018 Matt Hoyle
+# MIT License Copyright (c) 2022 mhio
