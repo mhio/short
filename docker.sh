ebfecb00fd44a47e6aec0d6addec19d5f69b8786
mhio <37205532+mhio@users.noreply.github.com>
2022-09-19 02:28:27 +0000

    Add find child and ls commands
    fci finds children of an image (for deleting)
    lsi/lsc lists and sorts

--- docker.sh
+++ docker.sh
@@ -21,18 +21,33 @@ dockerShort(){
         docker image "$@"
       fi
     ;;
-    n|ne|net|netw|netwo)    docker network "$@";;
-    v|vo|vol|volu|volum)    docker volume "$@";;
-    psa)      docker ps -a "$@";;
+    n|ne|net|netw|netwo)
+      docker network "$@"
+    ;;
+    v|vo|vol|volu|volum)
+      docker volume "$@"
+    ;;
+    psa)
+      docker ps -a "$@";;
     rmin)     d_images=$(docker images | awk '/<none>/ { print $3 }')
               [ -n "$d_images" ] && echo "$d_images" | xargs docker rmi
     ;;
     rme)      d_containers=$(docker ps -a | awk '/Exited|Created/ { print $1 }')
               [ -n "$d_containers" ] && echo "$d_containers" | xargs docker rm
     ;;
-    ri|rirm)  docker run -ti --rm "$@"
+    ri|rirm)
+      docker run -ti --rm "$@"
     ;;
-    h)        echo "the (d)ocker function - d (b r c i n v psa rmin rme ri)"
+    fci)
+      dockerChildImages "$@"
+    ;;
+    lsi)
+      docker images ls "$@" |  sort
+    ;;
+    lsc)
+      docker container ls "$@" | sort
+    ;;
+    h)        echo "the (d)ocker function - d (b r c i n v psa rmin rme ri fci)"
               echo " d b(uild)"
               echo " d r(un)"
               echo " d c(ommit)"
@@ -42,9 +57,19 @@ dockerShort(){
               echo " d psa (ps -a)"
               echo " d rmin (rmi all <none>)"
               echo " d rme (rm all exited/created)"
-              echo " d ri (run interacthive and remove afrer)"
+              echo " d ri (run interactive and remove after)"
+              echo " d fci (find child images referencing \$image_id)"
+              echo " d lsi (list images)"
+              echo " d lsc (list containers)"
     ;;
     *)        docker "$local_docker_arg" "$@";;
   esac
 }
 
+# Find all references to an image id (docker rmi failures)
+dockerChildImages(){
+  docker_child_images_image_id="$1"
+  docker images --quiet --filter "since=${docker_child_images_image_id}" \
+   | xargs -I {} sh -c "docker history --quiet {} | grep ${docker_child_images_image_id} && echo {}" \
+   | sort -u
+}
