c11bece3be793308b79032afd097034b8fbad12e
mhio <37205532+mhio@users.noreply.github.com>
2023-10-05 04:34:47 +0000

    add commit last message

--- git.sh
+++ git.sh
@@ -14,7 +14,9 @@ gitShort(){
     c|co|com|comm|commi) git commit "$@";;
     ca) git commit --amend "$@";;
     ch|che|chec|check|checko|checkou) git checkout "$@";;
+    cl|clm) git commit -eF "$(git rev-parse --git-dir)/COMMIT_EDITMSG" "$@";;
     d|di|dif) git diff "$@";;
+    f|fe|fet|fetc) git fetch "$@";;
     l|lo) git log "$@";;
     p|pu|pul) git pull "$@";;
     ps|psh|pus) git push "$@";;
@@ -25,13 +27,15 @@ gitShort(){
     ln|logname) git log --name-status "$@";;
 
     # Help
-    h) echo "the (g)it function - g (a b c ch d l p ps s t)"
+    h) echo "the (g)it function - g (a b c ca ch cl d f l ln p ps s t)"
        echo " g a(dd)"
        echo " g b(ranch)"
        echo " g c(ommit)"
        echo " g ca (commit --amend)"
+       echo " g clm (commit with last message)"
        echo " g ch(eckout)"
        echo " g d(iff)"
+       echo " g f(etch)"
        echo " g h (print g help)"
        echo " g l(og)"
        echo " g ln (log --name-status)"
